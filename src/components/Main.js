d3d51fce66b912b57f6b38641d42502f6dd63aac
Chase-Owens <chasejonathanowens@gmail.com>
2018-09-13 14:58:24 -0500

    functionalMainComponent

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import List from "./List";
 import Map from "./Map";
 
-class Main extends Component {
-  state = {};
-  render() {
-    let { discoveriesDisplayed, mode, styles } = this.props;
-    return (
-      <main className="main-div">
-        <List discoveriesDisplayed={discoveriesDisplayed} />
-        <Map
-          discoveriesDisplayed={discoveriesDisplayed}
-          mode={mode}
-          styles={styles}
-        />
-      </main>
-    );
-  }
-}
+const Main = ({ discoveriesDisplayed, mode, styles }) => {
+  return (
+    <main className="main-div">
+      <List discoveriesDisplayed={discoveriesDisplayed} />
+      <Map
+        discoveriesDisplayed={discoveriesDisplayed}
+        mode={mode}
+        styles={styles}
+      />
+    </main>
+  );
+};
 
 export default Main;
